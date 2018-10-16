8a7b286417f192084b2f41372c5841891cdcee7e
dickmao <dick.r.chiang@gmail.com>
2018-10-15 23:27:25 -0400

    good, ein:%notebooklist% is still good after all the up and down

--- features/step-definitions/ein-steps.el
+++ features/step-definitions/ein-steps.el
@@ -28,19 +28,20 @@
 (When "^new \\(.+\\) notebook$"
       (lambda (kernel)
         (multiple-value-bind (url-or-port token) (ein:jupyter-server-conn-info)
-          (lexical-let ((ks (ein:get-kernelspec url-or-port kernel)) notebook)
-            (ein:notebooklist-new-notebook url-or-port ks nil
-                (lambda (nb created &rest ignore)
-                  (setq notebook nb)))
-            (ein:testing-wait-until (lambda () (and notebook
-                                                    (ein:aand (ein:$notebook-kernel notebook)
-                                                              (ein:kernel-live-p it))))
-                                    nil 10000 2000)
-            (let ((buf-name (format ein:notebook-buffer-name-template
-                                    (ein:$notebook-url-or-port notebook)
-                                    (ein:$notebook-notebook-name notebook))))
-              (switch-to-buffer buf-name)
-              (Then "I should be in buffer \"%s\"" buf-name))))))
+          (with-current-buffer (ein:notebooklist-get-buffer url-or-port)
+            (lexical-let ((ks (ein:get-kernelspec url-or-port kernel)) notebook)
+              (ein:notebooklist-new-notebook url-or-port ks nil
+                                             (lambda (nb created &rest ignore)
+                                               (setq notebook nb)))
+              (ein:testing-wait-until (lambda () (and notebook
+                                                      (ein:aand (ein:$notebook-kernel notebook)
+                                                                (ein:kernel-live-p it))))
+                                      nil 10000 2000)
+              (let ((buf-name (format ein:notebook-buffer-name-template
+                                      (ein:$notebook-url-or-port notebook)
+                                      (ein:$notebook-notebook-name notebook))))
+                (switch-to-buffer buf-name)
+                (Then "I should be in buffer \"%s\"" buf-name)))))))
 
 (When "^I open notebooklist$"
       (lambda ()
