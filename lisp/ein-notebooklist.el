ec70350004b88094f50a2453217153b38ea53208
dickmao <dick.r.chiang@gmail.com>
2018-10-19 17:05:55 -0400

    Revert "Address #345 too many open files"
    This reverts commit 022e8a77cde2117050b66f187736ad520010e9f7.

--- lisp/ein-notebooklist.el
+++ lisp/ein-notebooklist.el
@@ -220,6 +220,10 @@ PATH is specifying directory from file navigation.  PATH is empty on login.  RES
             (lexical-let ((d (deferred:new #'identity)))
               (ein:query-kernelspecs url-or-port (lambda ()
                                                    (deferred:callback-post d)))
+              d)
+            (lexical-let ((d (deferred:new #'identity)))
+              (ein:content-query-hierarchy url-or-port (lambda (tree)
+                                                         (deferred:callback-post d)))
               d))
           (deferred:nextc it
             (lambda (&rest ignore)
