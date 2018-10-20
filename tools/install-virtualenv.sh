e65b0cbc00215c3cb3a5456dcf5a31acec8ed438
dickmao <dick.r.chiang@gmail.com>
2018-10-19 20:30:25 -0400

    pyenv install

--- tools/install-virtualenv.sh
+++ tools/install-virtualenv.sh
@@ -14,14 +14,15 @@ if [ "x$TRAVIS_OS_NAME" = "xosx" ]; then
     brew list python &>/dev/null || brew install python
     brew list python3 &>/dev/null || brew install python3
     brew install pyenv-virtualenv
-    pyenv version
 
     case "${TOXENV}" in
         py27)
-            pyenv virtualenv 2.7 py27
+            pyenv install 2.7.12
+            pyenv virtualenv 2.7.12 py27
             ;;
         py35)
-            pyenv virtualenv 3.5 py35
+            pyenv install 3.5.2
+            pyenv virtualenv 3.5.2 py35
             ;;
     esac
 fi
